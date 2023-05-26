94b3860a31d5e5dbbdc1e0620b5c0fcfb3774386
Robert Marko <robimarko@gmail.com>
2023-05-26 21:04:51 +0200

    package: kernel: qca-ssdk: add support for ipq60xx
    IPQ60xx series has a different codename so pass that along to SSDK,
    since PTP and swconfig feature flags are not unique to ipq807x just
    move them to the generic ones.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    Signed-off-by: Dirk Buchwalder <buchwalder@posteo.de>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -1,3 +1,28 @@
+define Device/FitImage
+	KERNEL_SUFFIX := -uImage.itb
+	KERNEL = kernel-bin | libdeflate-gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/FitImageLzma
+	KERNEL_SUFFIX := -uImage.itb
+	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/EmmcImage
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+
+define Device/UbiFit
+	KERNEL_IN_UBI := 1
+	IMAGES := factory.ubi sysupgrade.bin
+	IMAGE/factory.ubi := append-ubi
+	IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+
 define Device/linksys_mr7350
        $(call Device/FitImage)
        $(call Device/UbiFit)
